1c823722a7d4363b673db9b26573ea02d81d7d3e
YuMichay <julia.one.such@gmail.com>
2021-08-23 14:12:33 +0300

    fix: fixed formula for max function

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ exports.max = function max (array) {
   if (array == undefined || array.length == 0){
     return 0;
     }
-    return Math.min.apply(null, array);
+    return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
